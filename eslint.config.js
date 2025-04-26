497781d93a01c1958814eca884c37507512a41f5
Erick Vargas <erianvc.dev@gmail.com>
2025-04-26 01:18:43 -0500

    refactor: moves linter to the root project

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,19 @@
-import eslintConfigCustom from 'eslint-config-custom'
+import love from 'eslint-config-love'
+import astro from 'eslint-plugin-astro'
 
-export default eslintConfigCustom
+export default [
+  ...astro.configs.recommended,
+  {
+    ...love,
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      ...love.rules,
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/prefer-destructuring': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+      '@typescript-eslint/no-unnecessary-type-parameters': 'off',
+      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+    },
+  },
+]
