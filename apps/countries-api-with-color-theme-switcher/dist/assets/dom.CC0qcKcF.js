4dbab91710cb84504b2dce3a3c3366a472fae910
Erick Vargas <mail@erian.dev>
2026-06-19 13:46:06 -0500

    feat: `countries-api-with-color-theme-switcher` no longer build

---
+++ apps/countries-api-with-color-theme-switcher/dist/assets/dom.CC0qcKcF.js
@@ -0,0 +1,1 @@
+const o=e=>document.querySelector(e),t=e=>document.querySelectorAll(e);export{o as $,t as a};
---
+++ apps/countries-api-with-color-theme-switcher/dist/assets/index.astro_astro_type_script_index_0_lang.CXwfjyUq.js
@@ -0,0 +1,1 @@
+import{$ as a,a as s}from"./dom.CC0qcKcF.js";const c=a(".cards"),o=Array.from(s(".card")),t=a("#search-input");export{c as a,o as c,t as s};
