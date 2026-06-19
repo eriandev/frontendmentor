1bbdd103e8219787b416cc5b5d0665ebaaa15cee
Erick Vargas <mail@erian.dev>
2026-06-18 20:05:57 -0500

    refactor: export interfaces from the components package

--- packages/shared/components/icon.svelte
+++ packages/shared/components/icon.svelte
@@ -1,15 +1,15 @@
+<script module lang="ts">
+  export interface IconProps extends SVGAttributes<SVGSVGElement> {
+    name: string
+    size?: number
+  }
+</script>
+
 <script lang="ts">
   import { InlineSvg } from '@usesvelte/inline-svg'
   import type { SVGAttributes } from 'svelte/elements'
 
-  interface Props extends SVGAttributes<SVGSVGElement> {
-    name: string
-    size?: number
-    width?: number
-    height?: number
-  }
-
-  const { name, size, width, height, ...restProps }: Props = $props()
+  const { name, size, width, height, ...restProps }: IconProps = $props()
 </script>
 
 <InlineSvg {name} width={size ?? width} height={size ?? width} {...restProps} />
